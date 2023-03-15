12c6fba8211be4b2032d08432e0dd669ed25ba79
Steven Weaver <sweaver@temple.edu>
2023-03-15 12:53:39 -0600

    adding background option and fractional sampling

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,31 @@ fn main() -> Result<(), Box<dyn Error>> {
         )
         .arg(
             Arg::new("number")
-                .help("Number of sequences to randomly sample")
+                .help("Number of sequences to randomly sample. If the number is less than 1, then it will be treated as a percentage of the dataset.")
                 .takes_value(true)
                 .required(false)
                 .short('n')
                 .long("number"),
         )
+        .arg(
+            Arg::new("store-background")
+                .help("Write the samples not selected randomly in a separate file.")
+                .takes_value(true)
+                .required(false)
+                .short('i')
+                .long("store-background"),
+        )
         .get_matches();
+
+    let mut store_background = Some("");
+
+    if !matches.value_of("store-background").is_none() {
+        store_background = Some(matches.value_of("store-background").unwrap());
+    }
 
     crate::process::process(
         matches.value_of("fasta").unwrap(),
         matches.value_of("number").unwrap(),
+        store_background.unwrap(),
     )
 }
--- src/process.rs
+++ src/process.rs
@@ -2,38 +2,88 @@ use bio::io::fasta;
 use std::error::Error;
 use std::ffi::OsStr;
 use std::io;
+use std::fs;
+use std::fs::OpenOptions;
 use std::path::Path;
 use std::str;
 use rand::{seq::IteratorRandom, thread_rng};
 
-// String Match Option
-pub fn random_sample<P: AsRef<Path> + AsRef<OsStr>>(filename: P, number: usize) -> bool {
+fn is_path_empty<P: AsRef<Path>>(path: P) -> bool {
+    return path.as_ref().to_string_lossy().to_string().is_empty()
+}
 
+// String Match Option
+pub fn random_sample<P: AsRef<Path> + AsRef<OsStr>>(filename: P, number: f32, store_background: P) -> bool {
+
+    let mut null_file = OpenOptions::new()
+    .write(true)
+    .open("/dev/null")
+    .unwrap();
+
+    let mut background_writer = fasta::Writer::new(io::BufWriter::new(null_file));
     let mut rng = thread_rng();
 
     //FASTA file related
     let file = Path::new(&filename).to_str().unwrap();
+
     let records = fasta::Reader::from_file(file).unwrap().records();
+    let records2 = fasta::Reader::from_file(file).unwrap().records();
+    let records3 = fasta::Reader::from_file(file).unwrap().records();
+
+	// If store_background is set, then write to the file
+
+    if !is_path_empty(&store_background) { 
+        let background_filename = Path::new(&store_background).to_str().unwrap();
+        let bg_file = fs::File::create(background_filename).unwrap();
+        let handle = io::BufWriter::new(bg_file);
+        background_writer = fasta::Writer::new(handle);
+    }
 
     // write JSON file
     let mut writer = fasta::Writer::new(io::stdout());
 
-    let sampled_records = records.choose_multiple(&mut rng, number);
+    // Check if the number is less than 1, if so, then treat it as a percentage
+    let whole_number: usize;
 
-    for seqrec in sampled_records {
-
-        let record = seqrec.unwrap();
-
-        //let sequence_id_bytes = record.id();
-        //let sequence_description_bytes = record.desc().unwrap_or("");
-        //let entire_header_raw = [sequence_id_bytes, sequence_description_bytes].join(" ");
-        //let entire_header = entire_header_raw.trim();
-        //println!("I am {}!", entire_header);
-
-        writer
-            .write(record.id(), record.desc(), record.seq())
-            .expect("Error writing record.");
+    if number < 1.0 {
+        //Get proprotion
+        let count: f32 = records3.count() as f32;
+        whole_number = (count * number) as usize;
+    } else {
+        whole_number = number as usize;
     }
+
+    let sampled_records = records.choose_multiple(&mut rng, whole_number);
+
+	let sampled_record_ids:String = sampled_records.into_iter().map(|x| String::from(x.unwrap().id())).collect();
+
+    // Get records that are note part of choose
+	for record in records2 {
+
+		let record = record.unwrap();
+
+		if sampled_record_ids.contains(record.id()) {
+
+			//let sequence_id_bytes = record.id();
+			//let sequence_description_bytes = record.desc().unwrap_or("");
+			//let entire_header_raw = [sequence_id_bytes, sequence_description_bytes].join(" ");
+			//let entire_header = entire_header_raw.trim();
+			//println!("I am {}!", entire_header);
+
+			writer
+				.write(record.id(), record.desc(), record.seq())
+				.expect("Error writing record.");
+
+		} else {
+
+            background_writer
+                .write(record.id(), record.desc(), record.seq())
+				.expect("Error writing record.");
+
+
+        }
+
+	}
 
     return true;
 
@@ -41,9 +91,10 @@ pub fn random_sample<P: AsRef<Path> + AsRef<OsStr>>(filename: P, number: usize) 
 
 pub(crate) fn process<P: AsRef<Path> + AsRef<OsStr>>(
     filename: P,
-    number_arg: &str
+    number_arg: &str,
+    store_background: P,
 ) -> Result<(), Box<dyn Error>> {
-    let number =  number_arg.parse::<usize>().unwrap();
-    random_sample(filename, number);
+    let number =  number_arg.parse::<f32>().unwrap();
+    random_sample(filename, number, store_background);
     Ok(())
 }
